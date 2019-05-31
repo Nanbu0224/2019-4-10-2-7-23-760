7e45a2471f55c83d8295f9492693b254fac8664f
Nanbu0224 <40815063+Nanbu0224@users.noreply.github.com>
2019-05-31 11:56:41 +0800

    Add files via upload

--- main/main.js
+++ main/main.js
@@ -1,4 +1,15 @@
-module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+module.exports = function main({distance:driveDistance,parkTime:waitTime}) {
+	var waitPrice = 0;
+	waitPrice = waitTime * 0.25;
+	var distancePrice = 0;
+	if (driveDistance > 0 && driveDistance< 2){
+		distancePrice = 6;
+	}
+	if (driveDistance >=2 && driveDistance <=8){
+		distancePrice = 6 + (driveDistance - 2) * 0.8;
+	}
+	if (driveDistance > 8) {
+		distancePrice = 6 + 4.8 + (driveDistance - 8) * 1.2;
+	}
+	return (Math.round(distancePrice+waitPrice));
 };
